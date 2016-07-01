8c79de76c6bd6d025ab89fadf45eb285bb5df137
Rebecca Monroy <rebemon95@outlook.com>
2016-07-01 03:54:56 -0600

    Initial Commit

---
+++ PruebaGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PruebaGit
+//
+//  Created by Monroy on 7/1/16.
+//  Copyright © 2016 Monroy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
